3a0e411784cda8c21d53e6129c4bf1ad83537e08
masted <masted311@gmail.com>
2014-04-25 15:30:28 +0400

    ...

--- config/customTypes.php
+++ config/customTypes.php
@@ -1,30 +1,4 @@
 <?php
 
 return [
-  'pageMaker' => [
-    'extends' => 'common',
-    'vhostAliases' => [
-      '/pageMaker/' => '{ngnEnvPath}/pageMaker/static/',
-    ]
-  ],
-  'sd'        => [
-    'noDb'         => true,
-    'vhostAliases' => [
-      '/sd/' => '/home/user/sd/static/'
-    ],
-    'afterCmdTttt' => 'php {runPath}/run.php site {name} sd/install'
-  ],
-  'sb'        => [
-    'vhostAliases' => [
-      '/sb/'  => '{ngnEnvPath}/sb/static/',
-      '/cpm/' => '{ngnEnvPath}/sb/lib/cpm/'
-    ],
-    'afterCmdTttt' => 'php {runPath}/run.php site {name} {ngnEnvPath}/sb/install'
-  ],
-  'sd-paralax'        => [
-    'extends' => 'sd',
-    'vhostAliases' => [
-      '/paralax/'  => '{ngnEnvPath}/sd-paralax/static/',
-    ]
-  ]
 ];
--- config/types.php
+++ config/types.php
@@ -10,4 +10,30 @@ return [
     ],
     'afterCmdTttt' => 'php {scriptsPath}/updateStartStopScripts.php'
   ],
+  'pageMaker' => [
+    'extends' => 'common',
+    'vhostAliases' => [
+      '/pageMaker/' => '{ngnEnvPath}/pageMaker/static/',
+    ]
+  ],
+  'sd'        => [
+    'noDb'         => true,
+    'vhostAliases' => [
+      '/sd/' => '{ngnEnvPath}/sd/static/'
+    ],
+    'afterCmdTttt' => 'php {runPath}/run.php site {name} sd/install'
+  ],
+  'sb'        => [
+    'vhostAliases' => [
+      '/sb/'  => '{ngnEnvPath}/sb/static/',
+      '/cpm/' => '{ngnEnvPath}/sb/lib/cpm/'
+    ],
+    'afterCmdTttt' => 'php {runPath}/run.php site {name} {ngnEnvPath}/sb/install'
+  ],
+  'sd-paralax'        => [
+    'extends' => 'sd',
+    'vhostAliases' => [
+      '/paralax/'  => '{ngnEnvPath}/sd-paralax/static/',
+    ]
+  ]
 ];
