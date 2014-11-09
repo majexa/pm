815e7becfe20c00027ac213b83586c03750c2ae4
masted <masted311@gmail.com>
2014-11-09 11:34:26 +0400

    Auto-commit from localhost

--- pm.php
+++ pm.php
@@ -2,6 +2,7 @@
 <?php
 
 define('PM_PATH', __DIR__);
-require __DIR__.'/common-init.php';//
+define('DATA_PATH', __DIR__.'/data');
+require __DIR__.'/common-init.php';
 Cli::storeCommand(__DIR__.'/logs');
 new PmManager($_SERVER['argv']);
