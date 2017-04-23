8d39d4edc00fdd4db43ce1c7bcda23b458ed9e9e
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-04-23 13:00:18 +0300

    fix record getter in cli tool

--- lib/project/PmLocalProjects.class.php
+++ lib/project/PmLocalProjects.class.php
@@ -6,10 +6,7 @@
 class PmLocalProjects extends CliAccessOptionsMultiWrapper {
 
   protected function records() {
-    return array_filter((new PmLocalProjectRecords)->getRecords(), function(array $record) {
-      if (!(new PmProjectConfig($record['name']))->isNgnProject()) return false;
-      return true;
-    });
+    return (new PmRecordsProject)->getArray();
   }
 
 }
