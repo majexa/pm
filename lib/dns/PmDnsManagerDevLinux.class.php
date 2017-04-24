1c5978828869f7337b995a4fafed50eda33eb4b2
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-04-24 09:55:08 +0300

    ...

--- lib/dns/PmDnsManagerDevLinux.class.php
+++ lib/dns/PmDnsManagerDevLinux.class.php
@@ -1,14 +1,20 @@
 <?php
 
+function sys2($cmd, $output = false) {
+  if ($output and !getConstant('OUTPUT_DISABLE')) output('Cmd: '.$cmd, $output);
+  exec($cmd, $output, $exitCode);
+  if ($exitCode) throw new Exception($output);
+  if ($output and !getConstant('OUTPUT_DISABLE') and $r) output("Cmd output: $r", $output);
+  return $output;
+}
+
 class PmDnsManagerDevLinux extends PmDnsManagerDevWin {
   
   protected $configFile = '/etc/hosts';
 
   protected function save(array $items) {
     $file = $this->_save($items);
-    print "BEFORE\n";
-    print `sudo cat $file > {$this->configFile}`;
-    print "AFTER\n";
+    sys2("sudo cat $file > {$this->configFile}");
   }
   
 }
