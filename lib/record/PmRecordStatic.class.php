f25e1eb64446c5fe4916f0d8fd2e1b4fde75ffd7
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-04-30 10:48:09 +0300

    add port customization in static projects for drone test

--- lib/record/PmRecordStatic.class.php
+++ lib/record/PmRecordStatic.class.php
@@ -2,11 +2,16 @@
 
 class PmRecordStatic extends PmRecordWritable {
 
+  function __construct(array $record) {
+    if (empty($record['port'])) $record['port'] = 80;
+    parent::__construct($record);
+  }
+
   protected function getVhostRecord() {
     Arr::checkEmpty($this->r, 'webroot');
     $vhostTttt = '
 server {
-  listen 80;
+  listen {port};
   server_name {domain};
   location / {
     root {webroot};
